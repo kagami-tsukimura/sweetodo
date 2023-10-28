76ad7f635bdc078d4febc29d4e7e53d14a68d31f
Kagami <shimura.kyohei7617@gmail.com>
2023-10-28 22:07:43 +0900

    update: sign up form format

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
-    <div className='text-green-500'>
+    <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <SignUp />
     </div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,29 @@
 import { FC } from 'react';
 
 const SignUp: FC = () => {
-  return <div>SignUp</div>;
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log('登録');
+  };
+
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name='email' type='email' placeholder='email' />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name='password' type='password' />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
