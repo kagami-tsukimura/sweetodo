0b2c5e59e2454fcedf0d21ad8259c5bc31dbfbb0
Kagami <shimura.kyohei7617@gmail.com>
2023-10-28 22:04:50 +0900

    update: tailwind test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import SignUp from './pages/SignUp';
 
-function App() {
-  return <div className='App'>test</div>;
-}
+const App = () => {
+  return (
+    <div className='text-green-500'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <SignUp />
+    </div>
+  );
+};
 
 export default App;
