47c359a25304b0bdb0f2f74010e344f349583c1a
Kagami <shimura.kyohei7617@gmail.com>
2023-10-31 22:42:00 +0900

    feat: dark mode hooks

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from 'react';
+
+type UseDarkMode = (isDark?: boolean) => {
+  isDarkMode: boolean;
+  toggle: (isDark?: boolean) => void;
+};
+
+export const useDarkMode: UseDarkMode = (isInitialDark = false) => {
+  const [isDarkMode, toggleTheme] = useState<boolean>(isInitialDark);
+  const toggle = useCallback(
+    (isDark?: boolean | ((prevState: boolean) => boolean)) => {
+      if (typeof isDark === 'undefined') {
+        toggleTheme((state) => !state);
+        return;
+      }
+
+      toggleTheme(isDark);
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  return { isDarkMode, toggle };
+};
