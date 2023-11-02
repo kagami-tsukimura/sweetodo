9692713949f871b1fee6784cc0a461bfc19cc49f
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 02:14:51 +0900

    feat: signin

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -7,10 +7,10 @@ const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  console.log(email, password);
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
+    console.log(email.value);
     createUserWithEmailAndPassword(auth, email.value, password.value)
       .then((userCredential) => {
         console.log('user created');
@@ -20,48 +20,80 @@ const SignUp = () => {
         alert(error.message);
         console.error(error);
       });
-    console.log(email.value);
-  };
-  const handleChangeEmail = (event: any) => {
-    setEmail(event.currentTarget.value);
-  };
-  const handleChangePassword = (event: any) => {
-    setPassword(event.currentTarget.value);
   };
 
   return (
-    <div>
-      <h1>サインアップ</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>メールアドレス</label>
-          <input
-            name='email'
-            type='email'
-            placeholder='email'
-            onChange={(event) => handleChangeEmail(event)}
-          />
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='password'
-            onChange={(event) => handleChangePassword(event)}
-          />
-        </div>
-        <hr />
-        <div>
-          <button>サインアップ</button>
-        </div>
-        <div>
-          <Link to={'/signin'}>
-            <button>サインイン</button>
+    <>
+      <section className='bg-gray-50 dark:bg-gray-900'>
+        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+          <Link
+            to={'/'}
+            className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+          >
+            SweeToDo
+            <img
+              className='w-8 h-8 ml-2'
+              src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/816db7fc-2284-cccc-e64e-54b96f8001a8.png'
+              alt='logo'
+            />
           </Link>
+          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+            <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+                Create an account
+              </h1>
+              <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor='email'
+                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    placeholder='example@penguin.com'
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor='password'
+                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  >
+                    Password
+                  </label>
+                  <input
+                    type='password'
+                    name='password'
+                    id='password'
+                    placeholder='••••••••'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                >
+                  Create an account
+                </button>
+                <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                  Already have an account?{' '}
+                  <Link
+                    to={'/signin'}
+                    className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                  >
+                    Login here
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
-      </form>
-    </div>
+      </section>
+    </>
   );
 };
 
