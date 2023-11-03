191e2ef334a9575b55cfbad24c7a341fa8da6c86
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 14:08:32 +0900

    fix: google signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -38,43 +38,45 @@ const SignIn = () => {
               />
             </Link>
             <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-              <form
-                onSubmit={handleSubmit}
-                className='p-6 space-y-4 md:space-y-6 sm:p-8'
-              >
-                <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-                  Sign in to your account
-                </h1>
-                <div>
-                  <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                    Your email
-                  </label>
-                  <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    placeholder='example@penguin.com'
-                  />
-                </div>
-                <div>
-                  <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                    Password
-                  </label>
-                  <input
-                    type='password'
-                    name='password'
-                    id='password'
-                    placeholder='••••••••'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  />
-                </div>
-                <button
-                  type='submit'
-                  className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+              <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+                <form
+                  onSubmit={handleSubmit}
+                  className='space-y-4 md:space-y-6'
                 >
-                  Sign in
-                </button>
+                  <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+                    Sign in to your account
+                  </h1>
+                  <div>
+                    <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                      Your email
+                    </label>
+                    <input
+                      type='email'
+                      name='email'
+                      id='email'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      placeholder='example@penguin.com'
+                    />
+                  </div>
+                  <div>
+                    <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                      Password
+                    </label>
+                    <input
+                      type='password'
+                      name='password'
+                      id='password'
+                      placeholder='••••••••'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    />
+                  </div>
+                  <button
+                    type='submit'
+                    className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                  >
+                    Sign in
+                  </button>
+                </form>
                 <GoogleSignin />
                 <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
                   Don’t have an account yet?{' '}
@@ -85,7 +87,7 @@ const SignIn = () => {
                     Sign up
                   </Link>
                 </p>
-              </form>
+              </div>
             </div>
           </div>
         </section>
