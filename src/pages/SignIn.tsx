4a95160a9ede5447217c212ffaa2b8c18c226c0c
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 11:10:39 +0900

    style: signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,7 @@
-import {
-  GoogleAuthProvider,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
+import GoogleSignin from '../components/GoogleSignin';
 import { auth } from '../firebase';
 
 const SignIn = () => {
@@ -18,45 +15,79 @@ const SignIn = () => {
         navigate('/');
       })
       .catch((error) => {
+        alert("I can't Sign in😢\nCould you check it for email or password?");
         console.error(error);
       });
-  };
-  const googleSubmit = () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      signInWithPopup(auth, provider);
-      navigate('/');
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
     <div>
-      <h1>サインイン</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>メールアドレス</label>
-          <input name='email' type='email' placeholder='email' />
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input name='password' type='password' placeholder='password' />
-        </div>
-        <hr />
-        <div>
-          <button>サインイン</button>
-        </div>
-        <hr />
-        <div>
-          <Link to={'/signup'}>
-            <button>サインアップ</button>
+      <section className='bg-gray-50 dark:bg-gray-900'>
+        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+          <Link
+            to={'/'}
+            className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+          >
+            SweeToDo
+            <img
+              className='w-8 h-8 ml-2'
+              src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/816db7fc-2284-cccc-e64e-54b96f8001a8.png'
+              alt='logo'
+            />
           </Link>
+          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+            <form
+              onSubmit={handleSubmit}
+              className='p-6 space-y-4 md:space-y-6 sm:p-8'
+            >
+              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+                Sign in to your account
+              </h1>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='example@penguin.com'
+                />
+              </div>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  placeholder='••••••••'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                />
+              </div>
+              <button
+                type='submit'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+              >
+                Sign in
+              </button>
+              <GoogleSignin />
+              <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                Don’t have an account yet?{' '}
+                <Link
+                  to={'/signup'}
+                  className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
-      </form>
-      <div>
-        <button onClick={googleSubmit}>サインイン for Google</button>
-      </div>
+      </section>
+      <div></div>
     </div>
   );
 };
