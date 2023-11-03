302b63477093c69c8f9018696b49cc222679935f
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 12:30:28 +0900

    update: sign in and out to fade in

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import GoogleSignin from '../components/GoogleSignin';
 import { auth } from '../firebase';
+import { FadeInBottom } from '../utils/FadeInBottom';
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -21,74 +22,75 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <section className='bg-gray-50 dark:bg-gray-900'>
-        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-          <Link
-            to={'/'}
-            className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
-          >
-            SweeToDo
-            <img
-              className='w-8 h-8 ml-2'
-              src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
-              alt='logo'
-            />
-          </Link>
-          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-            <form
-              onSubmit={handleSubmit}
-              className='p-6 space-y-4 md:space-y-6 sm:p-8'
+    <>
+      <FadeInBottom>
+        <section className='bg-gray-50 dark:bg-gray-900'>
+          <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+            <Link
+              to={'/'}
+              className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
             >
-              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-                Sign in to your account
-              </h1>
-              <div>
-                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Your email
-                </label>
-                <input
-                  type='email'
-                  name='email'
-                  id='email'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  placeholder='example@penguin.com'
-                />
-              </div>
-              <div>
-                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Password
-                </label>
-                <input
-                  type='password'
-                  name='password'
-                  id='password'
-                  placeholder='••••••••'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                />
-              </div>
-              <button
-                type='submit'
-                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+              SweeToDo
+              <img
+                className='w-8 h-8 ml-2'
+                src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
+                alt='logo'
+              />
+            </Link>
+            <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+              <form
+                onSubmit={handleSubmit}
+                className='p-6 space-y-4 md:space-y-6 sm:p-8'
               >
-                Sign in
-              </button>
-              <GoogleSignin />
-              <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
-                Don’t have an account yet?{' '}
-                <Link
-                  to={'/signup'}
-                  className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+                  Sign in to your account
+                </h1>
+                <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                    Your email
+                  </label>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    placeholder='example@penguin.com'
+                  />
+                </div>
+                <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                    Password
+                  </label>
+                  <input
+                    type='password'
+                    name='password'
+                    id='password'
+                    placeholder='••••••••'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
                 >
-                  Sign up
-                </Link>
-              </p>
-            </form>
+                  Sign in
+                </button>
+                <GoogleSignin />
+                <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                  Don’t have an account yet?{' '}
+                  <Link
+                    to={'/signup'}
+                    className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-      <div></div>
-    </div>
+        </section>
+      </FadeInBottom>
+    </>
   );
 };
 
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import GoogleSignin from '../components/GoogleSignin';
 import { auth } from '../firebase';
+import { FadeInBottom } from '../utils/FadeInBottom';
 
 const SignUp = () => {
   const handleSubmit = (event: any) => {
@@ -21,76 +22,81 @@ const SignUp = () => {
 
   return (
     <>
-      <section className='bg-gray-50 dark:bg-gray-900'>
-        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-          <Link
-            to={'/'}
-            className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
-          >
-            SweeToDo
-            <img
-              className='w-8 h-8 ml-2'
-              src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
-              alt='logo'
-            />
-          </Link>
-          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-            <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-                Create an account
-              </h1>
-              <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
-                <div>
-                  <label
-                    htmlFor='email'
-                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    placeholder='example@penguin.com'
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor='password'
-                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-                  >
-                    Password
-                  </label>
-                  <input
-                    type='password'
-                    name='password'
-                    id='password'
-                    placeholder='••••••••'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  />
-                </div>
-                <button
-                  type='submit'
-                  className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
-                >
+      <FadeInBottom>
+        <section className='bg-gray-50 dark:bg-gray-900'>
+          <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+            <Link
+              to={'/'}
+              className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+            >
+              SweeToDo
+              <img
+                className='w-8 h-8 ml-2'
+                src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
+                alt='logo'
+              />
+            </Link>
+            <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+              <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+                <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
                   Create an account
-                </button>
-                <GoogleSignin />
-                <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
-                  Already have an account?{' '}
-                  <Link
-                    to={'/signin'}
-                    className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                </h1>
+                <form
+                  className='space-y-4 md:space-y-6'
+                  onSubmit={handleSubmit}
+                >
+                  <div>
+                    <label
+                      htmlFor='email'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type='email'
+                      name='email'
+                      id='email'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      placeholder='example@penguin.com'
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor='password'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      Password
+                    </label>
+                    <input
+                      type='password'
+                      name='password'
+                      id='password'
+                      placeholder='••••••••'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    />
+                  </div>
+                  <button
+                    type='submit'
+                    className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
                   >
-                    Login here
-                  </Link>
-                </p>
-              </form>
+                    Create an account
+                  </button>
+                  <GoogleSignin />
+                  <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                    Already have an account?{' '}
+                    <Link
+                      to={'/signin'}
+                      className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                    >
+                      Login here
+                    </Link>
+                  </p>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </FadeInBottom>
     </>
   );
 };
