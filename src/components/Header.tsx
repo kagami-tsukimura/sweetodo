e09797b19475795c235b6f1f2478f105b1a1b6d4
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 22:13:23 +0900

    feat: Header Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,71 +1,94 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
+import SignOut from '../pages/SignOut';
 import DarkModeButton from './DarkModeButton';
 
 const Header = () => {
   const displayName = auth.currentUser?.displayName;
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const email = auth.currentUser?.email;
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  return (
-    <div className='bg-gray-50 dark:bg-gray-900'>
-      <DarkModeButton />
+  useEffect(() => {
+    const handleOutsideClick = (event: { target: any }) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+    window.addEventListener('mousedown', handleOutsideClick);
 
-      <nav className='bg-white border-gray-200 dark:bg-gray-900'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <a href='https://flowbite.com/' className='flex items-center'>
+    return () => {
+      window.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className='border-b-2 border-gray-150 dark:border-gray-800 bg-gray-50 dark:bg-gray-900'>
+      <nav>
+        <div className='w-full flex flex-wrap items-center justify-between mx-auto p-4'>
+          <Link to={'/'} className='flex items-center'>
             <img
-              src='https://flowbite.com/docs/images/logo.svg'
+              src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
               className='h-8 mr-3'
               alt='Flowbite Logo'
             />
             <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-              Flowbite
+              SweeToDo
             </span>
-          </a>
-          <div className='flex items-center md:order-2'>
+          </Link>
+          <div className='flex items-center justify-center mr-4  md:order-2'>
             <button
               type='button'
-              className='flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-blue-300 dark:focus:ring-gray-600'
+              className='text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600'
               id='user-menu-button'
               aria-expanded={isDropdownOpen}
               onClick={toggleDropdown}
               data-dropdown-toggle='user-dropdown'
               data-dropdown-placement='bottom'
             >
-              {auth.currentUser?.photoURL && (
+              {auth.currentUser?.photoURL ? (
                 <img
                   src={auth.currentUser.photoURL}
                   alt={displayName!}
                   style={{ display: auth.currentUser ? 'block' : 'none' }}
                   className='w-8 h-8 rounded-full hover:cursor-pointer'
                 />
+              ) : (
+                <img
+                  src='https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3292052/ee0d3a4c-a04d-5905-ea87-11986aebb0b6.png'
+                  alt={displayName!}
+                  style={{ display: auth.currentUser ? 'block' : 'none' }}
+                  className='w-8 h-8 rounded-full hover:cursor-pointer'
+                />
               )}
             </button>
+            <DarkModeButton />
 
             <div
+              ref={dropdownRef}
               className={`${
                 isDropdownOpen ? 'block' : 'hidden'
-              } z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`}
+              } absolute top-12 right-0 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`}
               id='user-dropdown'
             >
               <div className='px-4 py-3'>
-                <span className='block text-sm text-gray-900 dark:text-white'>
-                  Bonnie Green
+                <span className='text-center block text-sm text-gray-900 dark:text-white'>
+                  {displayName ? displayName : 'Email-User'}
                 </span>
-                <span className='block text-sm  text-gray-500 truncate dark:text-gray-400'>
-                  name@flowbite.com
+                <span className='text-center block text-sm  text-gray-500 truncate dark:text-gray-400'>
+                  {email}
                 </span>
               </div>
               <ul className='py-2' aria-labelledby='user-menu-button'>
                 <li>
                   <Link
                     to={'/'}
-                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
+                    className='text-center block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
                   >
                     Dashboard
                   </Link>
@@ -73,45 +96,16 @@ const Header = () => {
                 <li>
                   <Link
                     to={'/'}
-                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
+                    className='text-center block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
                   >
                     Settings
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    to={'/'}
-                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
-                  >
-                    Sign out
-                  </Link>
+                  <SignOut />
                 </li>
               </ul>
             </div>
-            <button
-              data-collapse-toggle='navbar-user'
-              type='button'
-              className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-              aria-controls='navbar-user'
-              aria-expanded='false'
-            >
-              <span className='sr-only'>Open main menu</span>
-              <svg
-                className='w-5 h-5'
-                aria-hidden='true'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 17 14'
-              >
-                <path
-                  stroke='currentColor'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M1 1h15M1 7h15M1 13h15'
-                />
-              </svg>
-            </button>
           </div>
           <div
             className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
