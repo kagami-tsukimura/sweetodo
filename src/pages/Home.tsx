90afa4706b73c17c209e087d5297affca5f2452e
Kagami <shimura.kyohei7617@gmail.com>
2023-11-03 16:28:58 +0900

    update: header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import Header from '../components/Header';
 import Logout from './Logout';
 
 const Home = () => {
   return (
     <div className='bg-gray-50 dark:bg-gray-900'>
+      <Header />
       <button className='button bg-red-200'>ボタン</button>
       <Logout />
     </div>
--- src/router/PageRouter.tsx
+++ src/router/PageRouter.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserType } from '../App';
-import Header from '../components/Header';
 import NotFound from '../pages/404';
 import Home from '../pages/Home';
 import Logout from '../pages/Logout';
@@ -14,7 +13,6 @@ type PageRouterProps = {
 const PageRouter = ({ user }: PageRouterProps) => {
   return (
     <div className="className='bg-gray-50 dark:bg-gray-900">
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={user ? <Home /> : <SignIn />} />
